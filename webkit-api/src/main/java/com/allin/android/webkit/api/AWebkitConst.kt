cff6036806a4362bb169c759822d4708ff123a46
xianxueliang <xianxueliang@allinmd.cn>
2019-12-24 22:39:58 +0800

    modify packageName

--- webkit-api/src/main/java/com/allin/android/webkit/api/AWebkitConst.kt
+++ webkit-api/src/main/java/com/allin/android/webkit/api/AWebkitConst.kt
@@ -1,6 +1,6 @@
 package com.allin.android.webkit.api
 
-const val GENERATED_PACKAGE_NAME = "com.allin.android.webkit.collector"
+const val GENERATED_PACKAGE_NAME = "com.allin.android.webkit.generate"
 const val AWEBKIT = "AWebkit"
 const val SUFFIX_JS_COLLECTOR = "\$JsApiCollector"
 const val SUFFIX_LIFECYCLE_REGISTRANT = "\$LifecycleRegistrant"
